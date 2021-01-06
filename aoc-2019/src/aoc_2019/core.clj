f416738badd73ee2da18c89bb220033e6d40106c
polarfoxgirl <helga.nikonova@gmail.com>
2021-01-05 20:13:16 -0500

    Day 1 part 1

--- aoc-2019/src/aoc_2019/core.clj
+++ aoc-2019/src/aoc_2019/core.clj
@@ -1,7 +1,8 @@
 (ns aoc-2019.core
   (:gen-class))
+(require 'aoc-2019.day-1.solution)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hey, World!"))
+  (println (aoc-2019.day-1.solution/solve)))
---
+++ aoc-2019/src/aoc_2019/day_1/solution.clj
@@ -0,0 +1,17 @@
+(ns aoc-2019.day-1.solution)
+
+(defn process-line
+  [line]
+  ( - (quot (Integer/parseInt line) 3) 2)
+)
+
+(defn solve
+  []
+  (do
+    (->>
+      (clojure.string/split-lines (slurp "src/aoc_2019/day_1/input"))
+      (map process-line)
+      (reduce +)
+    )
+  )
+)
